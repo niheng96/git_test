77309bdb1a3963b100e0bc6555e27bac0d3fc19e
niheng <1436126819@qq.com>
2018-11-18 21:20:41 +0800

    把本地演示工程添加到git

---
+++ git_test/src/sc916/GitTest.java
@@ -0,0 +1,13 @@
+package sc916;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+    }
+}
