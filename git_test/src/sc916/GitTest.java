3ed5c48669ab39abc049e99aee092a8eda087006
niheng <1436126819@qq.com>
2018-11-19 09:52:23 +0800

    添加一些方法

--- git_test/src/sc916/GitTest.java
+++ git_test/src/sc916/GitTest.java
@@ -9,5 +9,22 @@ public class GitTest {
         System.out.println("Hello git!");
         System.out.println("Hello git!");
         System.out.println("Hello git!");
+
+        //添加方法
+        method1();
+        new GitTest().method2();
     }
+
+    public static void method1() {
+        int i = 1;
+        i += 10;
+        System.out.println(i);
+    }
+
+    public void method2() {
+        int i = 1;
+        i += 10;
+        System.out.println(i);
+    }
+
 }
